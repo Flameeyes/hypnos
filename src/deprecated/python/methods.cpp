fdb6ffd293b0701dc6805419549861be4df52d66
Diego Elio Pettenò <flameeyes@flameeyes.eu>
2003-12-28 22:47:34 +0000

    Started python embedding

---
+++ src/deprecated/python/methods.cpp
@@ -0,0 +1,23 @@
+/*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*
+| PyUO Server Emulator                                                     |
+|                                                                          |
+| This software is free software released under GPL2 license.              |
+| You can find detailed license information in pyuo.cpp file.              |
+|                                                                          |
+| PyUO Server Emulator                                                     |
+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*/
+/*!
+\file
+\author Flameeyes & Kheru
+\brief Python methods declaration
+*/
+
+
+/*!
+\brief Methods for PyUO module
+
+This variable contains the list of methods that are passed to python
+*/
+static PyMethodDef PyUOMethods[] = {
+	{NULL, NULL, 0, NULL}
+};
