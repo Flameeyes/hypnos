b68030d4c6f2d8c78b401f42eae488dd7b4aa5e0
Diego Elio Pettenò <flameeyes@flameeyes.eu>
2003-12-29 12:48:20 +0000

    changelog.txt 	- Added some data about the changes between NoX-Wizard and PyUO
    doxygen
    	- Renamed the project
    
    common_libs.h, archs/pyunix.h, archs/pywin32.h
    	- Split-out also tthread in archs
    
    Deleted old useless files.

--- src/includes/pyunix.h
+++ src/includes/pyunix.h
@@ -52,4 +52,106 @@ void initclock();
 
 void Sleep(unsigned long msec);
 
+
+namespace tthreads {
+
+class Mutex {
+  private:
+        bool m_bLocked;
+        bool m_bDebug;
+        char* m_szMutexName;
+    	pthread_mutex_t* m_mutex;
+   inline void init(bool alreadylocked) {
+    	    m_mutex = (pthread_mutex_t *) malloc (sizeof (pthread_mutex_t));
+    	    pthread_mutex_init (m_mutex, NULL);
+        m_bLocked = false;
+        m_bDebug = false;
+        if (alreadylocked) enter();
+    }
+  public:
+    inline Mutex(bool alreadylocked = false) { init(alreadylocked); }
+    inline Mutex(char *name, bool alreadylocked = false) { init(alreadylocked); setDebugMode(name); }
+    inline ~Mutex() {
+        if (m_bDebug) {
+            printf("DBG-MUTEX: Mutex %s destroyed\n", m_szMutexName);
+            if (m_szMutexName!=NULL) delete[] m_szMutexName;
+        }
+    	    pthread_mutex_destroy (m_mutex);
+        	free(m_mutex);
+    }
+    inline void enter () {
+        if (m_bDebug) printf("DBG-MUTEX: Entering mutex %s\n", m_szMutexName);
+    	    pthread_mutex_lock(m_mutex);
+        m_bLocked = true;
+        if (m_bDebug) printf("DBG-MUTEX: Entered mutex %s\n", m_szMutexName);
+    }
+    inline void leave () {
+        if (m_bDebug) printf("DBG-MUTEX: Leaving mutex %s\n", m_szMutexName);
+        m_bLocked = false;
+        	pthread_mutex_unlock(m_mutex);
+    }
+    inline bool tryEnter () {
+        if (m_bDebug) printf("DBG-MUTEX: Try-Locking mutex %s\n", m_szMutexName);
+            return pthread_mutex_trylock(m_mutex)==0;
+    }
+    void setDebugMode (char *name) { m_bDebug = false; m_szMutexName = new char[strlen(name)+2]; strcpy(m_szMutexName, name);}
+};
+
+
+
+
+template <typename T> class Atomic {
+  protected:
+    Mutex m_mutex;
+    volatile T m_val;
+    inline void enterIfEq(const T& compval)  { while(true) { m_mutex.enter(); if (m_val==compval) return; m_mutex.leave(); Sleep(1); } }
+    inline void enterIfNEq(const T& compval) { while(true) { m_mutex.enter(); if (m_val!=compval) return; m_mutex.leave(); Sleep(1); } }
+  public:
+	inline T setIfEq  (const T& newval, const T& compval = 0) { enterIfEq(compval); T old = m_val; m_val = newval; m_mutex.leave(); return old;}
+	inline T setIfNEq (const T& newval, const T& compval = 0) { enterIfNEq(compval); T old = m_val; m_val = newval; m_mutex.leave();  return old;}
+	inline T decIfEq  (const T& newval = 1, const T& compval = 0) { enterIfEq(compval); T old = m_val; m_val -= newval; m_mutex.leave();  return old;}
+	inline T decIfNEq (const T& newval = 1, const T& compval = 0) { enterIfNEq(compval); T old = m_val; m_val -= newval; m_mutex.leave();  return old;}
+	inline T incIfEq  (const T& newval = 1, const T& compval = 0) { return dec (-newval, compval); }
+	inline T incIfNEq (const T& newval = 1, const T& compval = 0) { return dec (-newval, compval); }
+	inline T set      (const T& newval) { m_mutex.enter(); T old = m_val; m_val = newval; m_mutex.leave();  return old;}
+    inline T dec      (const T& newval = 1) { m_mutex.enter(); T old = m_val; m_val -= newval; m_mutex.leave();  return old;}
+    inline T inc      (const T& newval = 1) { return dec(-newval); }
+
+    inline Atomic<T>& operator++() { inc(); return *this;}
+    inline Atomic<T>& operator--() { dec(); return *this;}
+    inline Atomic<T>& operator+=(const T& x) { inc(x);  return *this;}
+    inline Atomic<T>& operator-=(const T& x) { dec(x);  return *this;}
+    inline Atomic<T>& operator=(const T& newval) { set(newval);  return *this;}
+
+    inline operator T() { return m_val; }
+    inline bool operator==(const T& v) { return (m_val==v); }
+};
+
+
+class Semaphore : public Atomic<int> {
+public :
+    Semaphore(int n = 1) { set(n); }
+    inline void enter() { decIfNEq(); }
+    inline bool tryEnter() {
+        m_mutex.enter();
+        if (m_val!=0) {
+            m_val--;
+            m_mutex.leave();
+            return true;
+        }
+        m_mutex.leave();
+        return false;
+    }
+    void exit() { inc(); }
+};
+
+
+
+#define TTHREAD void*
+#define EXIT_TTHREAD { return NULL; }
+
+int startTThread( TTHREAD ( *funk )( void * ), void* param = NULL );
+
+};
+
 #endif
--- src/includes/pywin32.h
+++ src/includes/pywin32.h
@@ -33,4 +33,128 @@ inline bool pollCloseRequests () { return false; }
 inline void setup_signals (){ return; }
 inline void start_signal_thread() {return;}
 
+namespace tthreads {
+
+class Mutex
+{
+private:
+	bool m_bLocked;
+	bool m_bDebug;
+	char* m_szMutexName;
+	CRITICAL_SECTION m_cs;
+	CRITICAL_SECTION m_cs2;
+	inline void init(bool alreadylocked)
+	{
+		InitializeCriticalSection(&m_cs);
+		InitializeCriticalSection(&m_cs2);
+		m_bLocked = false;
+		m_bDebug = false;
+		if (alreadylocked) enter();
+	}
+public:
+	inline Mutex(bool alreadylocked = false) { init(alreadylocked); }
+	inline Mutex(char *name, bool alreadylocked = false) { init(alreadylocked); setDebugMode(name); }
+	inline ~Mutex()
+	{
+		if (m_bDebug)
+		{
+			printf("DBG-MUTEX: Mutex %s destroyed\n", m_szMutexName);
+			if (m_szMutexName!=NULL) delete[] m_szMutexName;
+        	}
+		DeleteCriticalSection(&m_cs);
+		DeleteCriticalSection(&m_cs2);
+	}
+	inline void enter ()
+	{
+		if (m_bDebug) printf("DBG-MUTEX: Entering mutex %s\n", m_szMutexName);
+		EnterCriticalSection(&m_cs2);
+		EnterCriticalSection(&m_cs);
+		m_bLocked = true;
+		LeaveCriticalSection(&m_cs2);
+		if (m_bDebug) printf("DBG-MUTEX: Entered mutex %s\n", m_szMutexName);
+    	}
+	inline void leave ()
+	{
+		if (m_bDebug) printf("DBG-MUTEX: Leaving mutex %s\n", m_szMutexName);
+		m_bLocked = false;
+		LeaveCriticalSection(&m_cs);
+	}
+	inline bool tryEnter ()
+	{
+		if (m_bDebug) printf("DBG-MUTEX: Try-Locking mutex %s\n", m_szMutexName);
+		//if (getOSVersion()==OSVER_WINNT) return TryEnterCriticalSection(&m_cs);
+		// Xan : workaround for TryEnterCriticalSection not working in Win9x
+		EnterCriticalSection(&m_cs2);
+		//we're here, we've control of m_bLocked.
+		if (m_bLocked) {
+				LeaveCriticalSection(&m_cs2);
+				return false;
+		}
+		// mmm so we'll enter!, thx to critsections are recursive.
+		enter();
+		LeaveCriticalSection(&m_cs2);
+		return true;
+	}
+	void setDebugMode (char *name)
+	{
+		m_bDebug = false;
+		m_szMutexName = new char[strlen(name)+2];
+		strcpy(m_szMutexName, name);
+	}
+};
+
+template <typename T> class Atomic {
+  protected:
+    Mutex m_mutex;
+    volatile T m_val;
+    inline void enterIfEq(const T& compval)  { while(true) { m_mutex.enter(); if (m_val==compval) return; m_mutex.leave(); Sleep(1); } }
+    inline void enterIfNEq(const T& compval) { while(true) { m_mutex.enter(); if (m_val!=compval) return; m_mutex.leave(); Sleep(1); } }
+  public:
+	inline T setIfEq  (const T& newval, const T& compval = 0) { enterIfEq(compval); T old = m_val; m_val = newval; m_mutex.leave(); return old;}
+	inline T setIfNEq (const T& newval, const T& compval = 0) { enterIfNEq(compval); T old = m_val; m_val = newval; m_mutex.leave();  return old;}
+	inline T decIfEq  (const T& newval = 1, const T& compval = 0) { enterIfEq(compval); T old = m_val; m_val -= newval; m_mutex.leave();  return old;}
+	inline T decIfNEq (const T& newval = 1, const T& compval = 0) { enterIfNEq(compval); T old = m_val; m_val -= newval; m_mutex.leave();  return old;}
+	inline T incIfEq  (const T& newval = 1, const T& compval = 0) { return dec (-newval, compval); }
+	inline T incIfNEq (const T& newval = 1, const T& compval = 0) { return dec (-newval, compval); }
+	inline T set      (const T& newval) { m_mutex.enter(); T old = m_val; m_val = newval; m_mutex.leave();  return old;}
+    inline T dec      (const T& newval = 1) { m_mutex.enter(); T old = m_val; m_val -= newval; m_mutex.leave();  return old;}
+    inline T inc      (const T& newval = 1) { return dec(-newval); }
+
+    inline Atomic<T>& operator++() { inc(); return *this;}
+    inline Atomic<T>& operator--() { dec(); return *this;}
+    inline Atomic<T>& operator+=(const T& x) { inc(x);  return *this;}
+    inline Atomic<T>& operator-=(const T& x) { dec(x);  return *this;}
+    inline Atomic<T>& operator=(const T& newval) { set(newval);  return *this;}
+
+    inline operator T() { return m_val; }
+    inline bool operator==(const T& v) { return (m_val==v); }
+};
+
+
+class Semaphore : public Atomic<int> {
+public :
+    Semaphore(int n = 1) { set(n); }
+    inline void enter() { decIfNEq(); }
+    inline bool tryEnter() {
+        m_mutex.enter();
+        if (m_val!=0) {
+            m_val--;
+            m_mutex.leave();
+            return true;
+        }
+        m_mutex.leave();
+        return false;
+    }
+    void exit() { inc(); }
+};
+
+
+
+#define TTHREAD void
+#define EXIT_TTHREAD { return; }
+
+int startTThread( TTHREAD ( *funk )( void * ), void* param = NULL );
+
+};
+
 #endif
