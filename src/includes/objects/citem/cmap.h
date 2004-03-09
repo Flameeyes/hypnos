c43d645a8a33c5e61f198f85ee60239f100d3953
Davide Poli <chronodt@users.berlios.de>
2004-03-09 20:34:31 +0000

    added cMap as descendant of cItem for map and map-related functions

---
+++ src/includes/objects/citem/cmap.h
@@ -0,0 +1,69 @@
+/*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*
+| PyUO Server Emulator                                                     |
+|                                                                          |
+| This software is free software released under GPL2 license.              |
+| You can find detailed license information in pyuo.cpp file.              |
+|                                                                          |
+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*/
+/*!
+\file
+\brief Header defining cContainer class
+*/
+#ifndef __MAPS_H
+#define __MAPS_H
+
+class cMap;
+typedef cMap* pMap;
+
+#include "objects/citem.h"
+#include "rtti.h"
+#include "typedefs.h"
+
+/*!
+\brief Map item
+\note This class must be identified via rtti() method to be sure
+that the item is a map
+*/
+class cMap : public cItem
+{
+	protected:
+        	struct pindataobject		        //!< this structure will hold x and y positions of a single pin in map
+                {
+                	UI16 x;
+                        UI16 y;
+                }
+                std::vector<pindataobject> pinData;    	//!< this vector will hold all the pins data (= their placement in the map)
+                bool writeable;				//!< map pins can be altered?
+               	inline pindataobject getPin(int pin)	//!< Gets pin data for pin number "pin" (pin goes from 1 to 50, as it appears on client)
+                	{ return pinData[pin - 1]; }
+        public:
+        	cMap(SERIAL ser);
+                cMap &operator = (const cMap &oldmap); 	//!< assignment operator = (for duping maps)
+
+               	inline const UI32 rtti() const          //!< returns the right rtti
+		{ return rtti::cMap; }
+
+
+                bool addPin(UI16 x, UI16 y);		//!< Adds a new pin to the map
+                bool insertPin(UI16 x,UI16 y, int pin); //!< Inserts a pin between 2 existing ones. existing pins >= pin get shifted by one
+                bool changePin(UI16 x,UI16 y, int pin);	//!< Moves pin to another position
+                bool removePin(int pin);		//!< Removes pin
+                bool clearAllPins();			//!< Removes all pins
+                bool toggleWritable();			//!< Toggle pin addability and replies to client actual writeability status
+
+
+                inline UI16 getX(int pin)               //!< Gets x "coordinate" for pin number "pin" (1<= pin <= 50)
+			{ return pinData[pin - 1].x; }
+                inline UI16 getY(int pin)               //!< Gets y "coordinate" for pin number "pin" (1<= pin <= 50)
+			{ return pinData[pin - 1].y; }
+		inline int getPinsNumber()              //!< Returns number of pins in map
+                	{ return pinData.size(); }
+
+
+                inline bool isBlankMap()
+	                { return type == ITYPE_BLANK_MAP; }
+                inline bool isWrittenMap()
+        	        { return type == ITYPE_MAP; }
+                inline bool isTreasureMap()
+                	{ return (type == ITYPE_TREASURE_MAP) || (type == ITYPE_DECIPHERED_MAP); }
+}
---
+++ src/src/objects/citem/cmap.cpp
@@ -0,0 +1,84 @@
+/*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*
+| PyUO Server Emulator                                                     |
+|                                                                          |
+| This software is free software released under GPL2 license.              |
+| You can find detailed license information in pyuo.cpp file.              |
+|                                                                          |
+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*/
+/*!
+\file
+\brief Implementation of cMap class
+*/
+
+#include "cmap.h"
+
+/*!
+\brief cMap constructor
+\param ser serial number
+*/
+cMap::cMap(SERIAL ser) : cItem(ser)
+{
+	writeable = true;
+//        pinData.clear();  //should not be necessary
+}
+
+
+cMap &operator = (const cMap &oldmap)
+{
+        pinData.clear();	//if assigned map was not "empty" (without pins), now it is :D
+
+       	//Now copying pindata to the new map
+        for(int i = 1; i <= oldmap.getPinsNumber(); i++) pinData.push_back(oldmap.getPin(int pin))
+        *((pItem)this) = (cItem)oldmap; //using the parent operator = function to copy all other parameters
+	return *this;
+}
+
+
+/*!
+\brief Adds a new pin to the map
+*/
+
+
+bool cMap::addPin(UI16 x, UI16 y)
+{
+	if ((pin > 50) || !writeable) return false;
+	pinData.push_back(pindataobject(x,y));
+        return true;
+}
+
+bool cMap::insertPin(UI16 x,UI16 y, int pin) 	//!< Inserts a pin between 2 existing ones. existing pins >= pin get shifted by one
+{
+	if (pin >= pinData.size() || !writeable) return false;
+	pinData.insert(pin - 1, pindataobject(x,y));
+        return true;
+}
+
+bool cMap::changePin(UI16 x,UI16 y, int pin)	//!< Moves pin to another position
+{
+	if (pin > pinData.size() || !writeable) return false;
+	pinData[pin - 1] = pindataobject(x,y);
+        return true;
+}
+
+bool cMap::removePin(int pin)		//!< Removes pin
+{
+	if ((pin < 1) || (pin > pinData.size()) || !writeable ) return false;
+        pinData.erase(pinData.begin() + pin - 1);
+        return true;
+}
+
+bool cMap::clearAllPins()		//!< Removes all pins
+{
+	if (!writeable) return false;
+	pinData.clear();
+        return true;
+}
+
+bool cMap::toggleWritable()		//!< Toggle pin addability and replies to client actual writeability status
+{
+	if (!isTreasureMap()) writeable = !writeable;
+        else writeable = false;
+      	cPacketSendMapPlotCourse pk(this, WriteableStatus, writeable, iter->x, iter->y);
+	client->sendPacket(&pk);
+        return true;
+}
