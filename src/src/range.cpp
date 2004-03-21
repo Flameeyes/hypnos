289f815c4ba176f91fea496f84454d05fe2958cd
Diego Elio Pettenò <flameeyes@flameeyes.eu>
2004-03-21 11:45:55 +0000

    Need to chop down range.[cpp|h] stuff.. moved to inlines and documented with deprecated, now grepping to find them out...

--- src/src/range.cpp
+++ src/src/range.cpp
@@ -18,10 +18,10 @@
 \author Luxor
 \brief Returns distance between two points.
 */
-R64 dist( Location a, Location b, LOGICAL countZ )
+const R64 dist( const Location a, const Location b, bool countZ )
 {
-        SI32 xDiff = a.x - b.x;
-        SI32 yDiff = a.y - b.y;
+        SI16 xDiff = a.x - b.x;
+        SI16 yDiff = a.y - b.y;
 	R64 distance = hypot( abs( xDiff ), abs( yDiff ) );
 	if ( !countZ || a.z == b.z )
 		return distance;
@@ -30,44 +30,94 @@ R64 dist( Location a, Location b, LOGICAL countZ )
 	return hypot( distance, distZ );
 }
 
-LOGICAL inRange(Location a, Location b, UI32 range)
+/*!
+\brief Check if a location is in the range of another one
+\param a Base location to check
+\param b Location to check if in range
+\param range range to use
+*/
+inline const bool inRange(const Location a, const Location b, const UI16 range = VISRANGE)
 {
 	return (dist(a,b)<= range);
 }
 
-LOGICAL inVisRange(Location a, Location b)
+/*!
+\brief Check if a location is in the visual range of another one
+\param a Base location to check
+\param b Location to check if in range
+\deprecated Should be changed to inRange() function with default range paramter
+*/
+inline const bool inVisRange(const Location a, const Location b)
 {
 	return (dist(a,b)<=VISRANGE);
 }
 
-LOGICAL char_inVisRange(P_CHAR a, P_CHAR b)
+/*!
+\brief Check if two characters are in visual range
+\param a First char to check
+\param b Second char to check
+\deprecated Should be changed to inRange() function with default range paramter
+*/
+inline const bool char_inVisRange(pChar a, pChar b)
 {
 	 return char_inRange( a, b, VISRANGE );
 }
 
-LOGICAL item_inVisRange(P_CHAR a, P_ITEM b )
+/*!
+\brief Check if an item is in the visual range of a char
+\param a Char to check the range of
+\param b Item to check if in range
+\depreated Should be changed to inRange() function with default range parameter
+*/
+inline const bool item_inVisRange(pChar a, pItem b )
 {
 	 return item_inRange( a, b, VISRANGE );
 }
 
-LOGICAL char_inRange(P_CHAR a, P_CHAR b, UI32 range)
+/*!
+\brief Check if two characters are in a definited range
+\param a First char
+\param b Second char
+\param range Range to check for
+*/
+inline const bool inRange(pChar a, pChar b, UI16 range = VISRANGE)
 {
-	VALIDATEPCR(a, false);
+	if ( ! a || ! b )
+		return false;
 	return ( a->distFrom( b ) <= range );
 }
 
-LOGICAL item_inRange(P_CHAR a, P_ITEM b, UI32 range)
+/*!
+\brief Check if an item is in the range of character
+\param a The char
+\param b The item
+\param range Range to check for
+*/
+inline const bool inRange(pChar a, pItem b, UI16 range = VISRANGE)
 {
-	VALIDATEPCR(a, false);
+	if( ! a || ! b )
+		return false;
 	return ( a->distFrom( b ) <= range );
 }
 
+/*!
+\brief Return the distance between a char and a item
+\param a The char
+\param b The item
+\deprecated use directly cChar::distFrom() function
+*/
 UI32 item_dist(P_CHAR a, P_ITEM b)
 {
 	VALIDATEPCR(a, VERY_VERY_FAR);
 	return a->distFrom( b );
 }
 
+/*!
+\brief Check if a char is near a banker
+\param i Character index
+\return true if in range, else false
+\todo Remove the index, use directly cChar
+*/
 LOGICAL inbankrange(int i)
 {
 	P_CHAR pc=MAKE_CHAR_REF(i);
