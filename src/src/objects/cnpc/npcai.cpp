16871a58ecdb2df56402df9394d2c4330bc4d572
Diego Elio Pettenò <flameeyes@flameeyes.eu>
2004-08-08 21:03:50 +0000

    Renamed PyUO into Hypnos

--- src/src/objects/cnpc/npcai.cpp
+++ src/src/objects/cnpc/npcai.cpp
@@ -1,5 +1,5 @@
 /*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*
-| PyUO Server Emulator                                                     |
+| Hypnos UO Server Emulator                                                |
 |                                                                          |
 | This software is free software released under GPL2 license.              |
 | You can find detailed license information in pyuo.cpp file.              |
