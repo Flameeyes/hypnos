5bf715f41030e9ce9d16ffc7f68fd58297791390
Diego Elio Pettenò <flameeyes@flameeyes.eu>
2004-08-29 01:23:08 +0000

    Merged misc and utils units.

--- src/includes/misc.h
+++ src/includes/misc.h
@@ -13,4 +13,18 @@ void npcattacktarget(pChar npc, pChar target);
 void callguards( int p );
 void usepotion(pChar pc, pItem pi);
 
+int checkBoundingBox(int xPos, int yPos, int fx1, int fy1, int fz1, int fx2, int fy2);
+int checkBoundingCircle(int xPos, int yPos, int fx1, int fy1, int fz1, int radius);
+int fielddir(pChar pc, int x, int y, int z);
+void npcsimpleattacktarget(int target2, int target);
+
+void endmessage(int x);
+void dooruse(pClient client, pItem pi/*int item*/);
+int calcGoodValue(pChar npc, pItem pi, int value,int goodtype); // by Magius(CHE) for trade system
+
+int whichbit( int number, int bit );
+int numbitsset( int number );
+
+std::string getSextantCoords(sPoint p, sPoint center)
+
 #endif
