adbec45aecb472c991c9fe31af31eaf89bbe710e
Diego Elio Pettenò <flameeyes@flameeyes.eu>
2004-10-04 18:06:45 +0000

    Misc fixes

--- src/src/objects/citem/cbook.cpp
+++ src/src/objects/citem/cbook.cpp
@@ -62,12 +62,12 @@ void cBook::changePages(char *packet, uint16_t p, uint16_t l, uint16_t size)
 	char s[34];
 	if ( p >= pages.size() )
 	{
-		WarnOut("Invalid page index in packet (index: %d max: %uz) [books.cpp]\n", p, pages.size());
+		WarnOut("Invalid page index in packet (index: %u max: %uz) [books.cpp]\n", p, pages.size());
 		return;
 	}
 	if ( l >= pages[p].size() )
 	{
-		WarnOut("Invalid line index in packet (index: %d max: %uz) [books.cpp]\n", l, pages[p].size());
+		WarnOut("Invalid line index in packet (index: %u max: %uz) [books.cpp]\n", l, pages[p].size());
 		return;
 	}
 
