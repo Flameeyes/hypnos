a5f3d477533f64011f83b4186f4948f4842f0533
Diego Elio Pettenò <flameeyes@flameeyes.eu>
2004-10-10 23:35:26 +0000

    Removed crosscompile script (no more needed now that we use autoconf/automake chain). Updated search and replace scripts to have them working right on svn and automake chain.

--- src/scripts/greplace.sh
+++ src/scripts/greplace.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-gsed -e "s/$1/$2/g" -i `grep "$1" * -rl | egrep -v 'errors.log|deprecated|doxyerror|\~|CVS|apidocs|Makefile|doc/'`
+gsed -e "s/$1/$2/g" -i `grep "$1" * -rl | egrep -v 'errors.log|deprecated|doxyerror|\~|.svn|apidocs|\.o|Makefile|doc|.libs|.deps'`
 
--- src/scripts/replace.sh
+++ src/scripts/replace.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-sed -e "s/$1/$2/g" -i `grep "$1" * -rl | egrep -v 'errors.log|deprecated|doxyerror|\~|CVS|apidocs|Makefile|doc/'`
+sed -e "s/$1/$2/g" -i `grep "$1" * -rl | egrep -v 'errors.log|deprecated|doxyerror|\~|.svn|apidocs|\.o|Makefile|doc|.libs|.deps'`
 
