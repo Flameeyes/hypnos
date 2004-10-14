7db80ffb0c83e90ce49a33bd919afd571b5bd204
Diego Elio Pettenò <flameeyes@flameeyes.eu>
2004-10-14 23:20:04 +0000

    Cleaned up version.h header

--- src/includes/version.h
+++ src/includes/version.h
@@ -4,6 +4,8 @@
 | This software is free software released under GPL2 license.              |
 | You can find detailed license information in hypnos.cpp file.            |
 |                                                                          |
+| Copyright (c) 2004 - Hypnos Project                                      |
+|                                                                          |
 *+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*/
 
 #ifndef __VERSION_H__
@@ -28,39 +30,6 @@ This is part of the client version control system.
 This is NOT necassairily the lastest client.
 Don't change it unless you are sure that its packets structure is in synch with network.cpp etc.
 */
-static const char *strSupportedClient = "3.0.3a";
+static const char *strSupportedClient = "4.0.3";
 
-#define OS    "UNKNOWN OS"
-#define OSFIX "[UNKNOWN OS]   "
-#define PLATFORMID 0
-
-#ifdef _CONSOLE
-#undef OS
-#undef OSFIX
-#undef PLATFORMID
-#define OS    ((ServerScp::g_nDeamonMode==0) ? ("Win32-Console") : ("WinNT-Service"))
-#define OSFIX ((ServerScp::g_nDeamonMode==0) ? ("[Win32-Console]") : ("[WinNT-Service]"))
-#endif
-
-#ifdef __unix__
-#undef OS
-#undef OSFIX
-#define OS    ((ServerScp::g_nDeamonMode==0) ? ("Linux-Console") : ("Linux-Deamon"))
-#define OSFIX ((ServerScp::g_nDeamonMode==0) ? ("[Linux-Console]") : ("[Linux-Daemon ]"))
-#endif
-
-#if defined(__OpenBSD__)
-#undef OS
-#undef OSFIX
-#define OS    ((ServerScp::g_nDeamonMode==0) ? ("OpenBSD-Console") : ("OpenBSD-Deamon"))
-#define OSFIX ((ServerScp::g_nDeamonMode==0) ? ("[OpenBSD tty]  ") : ("[OpenBSD daemn]"))
-#endif
-
-#if defined(__FreeBSD__)
-#undef OS
-#undef OSFIX
-#define OS    ((ServerScp::g_nDeamonMode==0) ? ("FreeBSD-Console") : ("FreeBSD-Deamon"))
-#define OSFIX ((ServerScp::g_nDeamonMode==0) ? ("[FreeBSD tty]  ") : ("[FreeBSD daemn]"))
-#endif
-
-#endif //__VERSION_H__
+#endif 
