8da313a90003f04bc6a20d57c186a828f031abfc
Diego Elio Pettenò <flameeyes@flameeyes.eu>
2004-11-10 13:13:32 +0000

    Removed debug output in fCliloc constructor.

--- src/libhypnos/muls/cliloc.cpp
+++ src/libhypnos/muls/cliloc.cpp
@@ -10,8 +10,6 @@
 
 #include "libhypnos/muls/cliloc.h"
 
-#include <iostream>
-
 namespace nLibhypnos {
 namespace nMULFiles {
 
@@ -22,8 +20,6 @@ namespace nMULFiles {
 fCliloc::fCliloc(std::string filename)
 	: file(1, filename)
 {
-	std::cerr << file.getArray() << '\t' << file.getSize() << std::endl;
-
 	char *ptr = reinterpret_cast<char*>(file.getArray())+6;
 	
 	char *end = (reinterpret_cast<char*>(file.getArray())+file.getCount());
@@ -34,18 +30,12 @@ fCliloc::fCliloc(std::string filename)
 		assert(ptr[4] == 0x00); // If it's not, need to check the format
 		uint16_t size = mtohs( *reinterpret_cast<uint16_t*>(ptr+5) );
 		
-		std::cerr << index << '\t' << size << '\t';
-		
 		char *buff = new char[size+1];
 		memcpy(buff, ptr+7, size);
 		buff[size] = '\0';
 		
-		std::cerr << buff << '\t';
-		
 		entries[index] = std::string(buff);
 		delete[] buff;
-		
-		std::cerr << entries.size() << std::endl;
 		
 		ptr += 7+size;
 	}
